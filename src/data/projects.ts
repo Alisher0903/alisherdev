7af201ad4c920fad912e2c951ee7c39131d9f4d8
Alisher0903 <alishersodiqov09@gmail.com>
2025-06-27 10:46:59 +0500

    image optimazation

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -52,7 +52,7 @@ export const PROJECT_SHOWCASE: ProjectShowcaseListItem[] = [
 export const PROJECTS_CARD: ProjectCardProps[] = [
   {
     name: "Sfera IT Solution",
-    favicon: "/images/projects/logos/solution.png",
+    favicon: "/images/projects/logos/solution.ico",
     imageUrl: [
       "/images/projects/sfera-solution.webp",
       "/images/projects/sferacard1.webp",
@@ -67,7 +67,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
   },
   {
     name: "Geodeziya",
-    favicon: "/images/projects/logos/geodezist.webp",
+    favicon: "/images/projects/logos/geodezist.ico",
     imageUrl: [
       "/images/projects/geodezistdark.webp",
       "/images/projects/geodezistlight.webp",
@@ -81,7 +81,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
   },
   {
     name: "Bookers",
-    favicon: "/images/projects/logos/bookers.png",
+    favicon: "/images/projects/logos/bookers.ico",
     imageUrl: [
       "/images/projects/bookerslight.webp",
       "/images/projects/bookersdark.webp",
@@ -94,7 +94,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     description:
       "This project is made for barbers to gather all barbers on one platform and systematize them, masters can accept online orders and help to maintain work schedules, and clients can also leave an online request to any master they want at any time.",
     sourceCodeHref: "https://github.com/Alisher0903/bookers-admin",
-    liveWebsiteHref: "http://bookers.uz",
+    liveWebsiteHref: "http://admin.bookers.uz:36080",
   },
   {
     name: "Sfera Academy",
@@ -124,12 +124,12 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     name: "Edu platform",
     favicon: "/images/projects/logos/sfera.ico",
     imageUrl: [
-      "/images/projects/sferaedu1.png",
-      "/images/projects/sferaedu2.png",
-      "/images/projects/sferaedu3.png",
-      "/images/projects/sferaedu4.png",
-      "/images/projects/sferaedu5.png",
-      "/images/projects/sferaedu6.png",
+      "/images/projects/sferaedu1.webp",
+      "/images/projects/sferaedu2.webp",
+      "/images/projects/sferaedu3.webp",
+      "/images/projects/sferaedu4.webp",
+      "/images/projects/sferaedu5.webp",
+      "/images/projects/sferaedu6.webp",
     ],
     description:
       "Platform courses are delivered by experienced instructors and combine theoretical knowledge with practical projects. Students benefit from real-world assignments, mentorship, and career support, including assistance with job placement. Graduates receive official certificates upon successful completion.",
@@ -139,7 +139,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
   {
     name: "Tunikafon",
     favicon: "/images/projects/logos/tunika.ico",
-    imageUrl: ["/images/projects/tunikafon.png"],
+    imageUrl: ["/images/projects/tunikafon.webp"],
     description:
       "Tunikafon is an admin panel developed to streamline operations in the industry. It helps manage employee tasks, calculate workloads, and track material usage efficiently through a centralized dashboard.",
     sourceCodeHref: "https://github.com/Alisher0903/tunikafon",
@@ -147,8 +147,8 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
   },
   {
     name: "Qr Pay",
-    favicon: "/images/projects/logos/qrpay.png",
-    imageUrl: ["/images/projects/qrpay.png", "/images/projects/qrpay2.png"],
+    favicon: "/images/projects/logos/qrpay.ico",
+    imageUrl: ["/images/projects/qrpay.webp", "/images/projects/qrpay2.webp"],
     description:
       "QRPay — A QR code-based payment system with a landing page (qrpay.uz) and an admin panel (my.qrpay.uz) for managing transactions.",
     sourceCodeHref: "https://github.com/Alisher0903/cross-pay-web",
@@ -156,11 +156,11 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
   },
   {
     name: "Railway",
-    favicon: "/images/projects/logos/railway.png",
+    favicon: "/images/projects/logos/railway.ico",
     imageUrl: [
-      "/images/projects/railway1.png",
-      "/images/projects/railway2.png",
-      "/images/projects/railway3.png",
+      "/images/projects/railway1.webp",
+      "/images/projects/railway2.webp",
+      "/images/projects/railway3.webp",
     ],
     description:
       "RAILWAY is a 3-panel site that gives you complete control over your railway workflow. This greatly facilitates the monitoring of employees, the accounting of used equipment and similar processes through the site.",
@@ -169,11 +169,11 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
   },
   {
     name: "GSR Logistic",
-    favicon: "/images/projects/logos/logistic.png",
+    favicon: "/images/projects/logos/logistic.ico",
     imageUrl: [
-      "/images/projects/gsrlogistic1.png",
-      "/images/projects/gsrlogistic2.png",
-      "/images/projects/gsrlogistic3.png",
+      "/images/projects/gsrlogistic1.webp",
+      "/images/projects/gsrlogistic2.webp",
+      "/images/projects/gsrlogistic3.webp",
     ],
     description:
       "GSR LOGISTIC is a website that controls and organizes bulk cargo transportation between Uzbekistan and China. Through this project, cargo control will be greatly strengthened, and the labor force will be enriched and facilitated. - in addition, the paperwork has been systematized with a much reduced amount. One of the useful aspects is that clients can track the location, status, and arrival of their orders online.",
@@ -184,9 +184,9 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     name: "Sfera Academy | Old",
     favicon: "/images/projects/logos/sfera.ico",
     imageUrl: [
-      "/images/projects/sfera-old.png",
-      "/images/projects/sfera-old1.png",
-      "/images/projects/sfera-old2.png",
+      "/images/projects/sfera-old.webp",
+      "/images/projects/sfera-old1.webp",
+      "/images/projects/sfera-old2.webp",
     ],
     description:
       "SFERA ACADEMY is a website thatprovides complete information about sfera academy, it is a site that providesinformation about courses such asbusiness training, programming courses frontend and backend, computer literacy, systemization.",
@@ -195,8 +195,8 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
   },
   {
     name: "Snake game",
-    favicon: "/images/projects/logos/snake.png",
-    imageUrl: ["/images/projects/snake.png"],
+    favicon: "/images/projects/logos/snake.ico",
+    imageUrl: ["/images/projects/snake.webp"],
     description:
       "Snake Game is a classic snake game built with React.js for desktop browsers. It features smooth movement, collision detection, and score tracking — all implemented using component-based logic and React state management.",
     sourceCodeHref: "https://github.com/Alisher0903/snake-game",
